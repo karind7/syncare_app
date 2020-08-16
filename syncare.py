abf0e84a32263350b56e7d43e378379d479a8f56
karin Dodis <karind7@gmail.com>
2020-08-16 22:09:55 +0300

    log in

--- syncare.py
+++ syncare.py
@@ -2,9 +2,11 @@ from firebase import firebase
 import pandas as pd
 import kivy
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
-
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 # firebase = firebase.FirebaseApplication('https://syncare-6b9b8.firebaseio.com/', None)
 # data = {'User name': 'mor',
@@ -15,17 +17,35 @@ from kivy.properties import ObjectProperty
 # for key in result:
 #     print(result[key])
 
-class MyGrid(GridLayout):
+
+class LoginWindow(Screen):
+
     user_name = ObjectProperty(None)
     password = ObjectProperty(None)
 
+    def reset(self):
+        self.user_name.text = ""
+        self.password.text = ""
+
     def log_in(self):
-        (User(self.user_name.text, self.password.text)).log_in()
+        print((User(self.user_name.text, self.password.text)).log_in())
+        self.reset()
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file("Syncare.kv")
+sm = WindowManager()
+screen = LoginWindow(name="login")
+sm.add_widget(screen)
+sm.current = "login"
 
 
 class Syncare(App):
     def build(self):
-        return MyGrid()
+        return sm
 
 
 class Patient:
@@ -48,7 +68,6 @@ class User:
               if data['Name'][i] == self.use_name and data['password '][i] == self.password:
                 return "user exsists"
             return "user dose not exsist"
-
 
     def __str__(self):
         return "user name: {} password: {}".format(self.use_name, self.password)
